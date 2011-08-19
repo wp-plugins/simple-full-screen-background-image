be0942ed953ce58795bc1e6fab32f1eef9d3bbdc
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-19 15:07:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-full-screen-background-image/trunk@425926 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/scripts.php
+++ includes/scripts.php
@@ -4,3 +4,11 @@ function fsb_load_css() {
 	wp_enqueue_style('fsb-image', plugin_dir_url( __FILE__ ) . 'fullscreen-image.css');
 }
 add_action('init', 'fsb_load_css');
+
+function fsb_load_admin_scripts() {
+	wp_enqueue_script( 'media-upload ');
+	wp_enqueue_script( 'thickbox' );
+}
+if(isset($_GET['page']) && $_GET['page'] == 'full-screen-background') {
+	add_action('admin_print_scripts', 'fsb_load_admin_scripts');
+}
